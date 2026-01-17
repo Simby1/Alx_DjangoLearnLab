65a7ec82ea0a2570fb251b6c8a8e101c7e082214
Simby1 <kimsimby@gmail.com>
2026-01-18 00:06:32 +0100

    refactor: Update URL patterns for clarity and consistency in views and authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
-from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    
-    # Authentication URLs
-    path('register/', register, name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    # Task 1 Views
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Task 2 Authentication URLs
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
