dd507ac0a9733a98c4342ac9dcd9c7ebe7ddae0e
Simby1 <kimsimby@gmail.com>
2026-01-18 22:34:30 +0100

    feat: Implement user roles and views for Admin, Librarian, and Member with corresponding templates- Implement Role-Based Access Control in Django - task3

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from relationship_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')), ]
+    path('', include('relationship_app.urls')), 
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    ]
