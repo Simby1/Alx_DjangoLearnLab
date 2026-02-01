6ac0242b0b65bee8f5fbf7ae628ccb8d22ba651c
Simby1 <kimsimby@gmail.com>
2026-02-02 00:08:58 +0100

    feat: Implement Authentication and Permissions in Django REST Framework - task 3

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,11 @@
-from rest_framework import viewsets, generics # keep generics if you want both
+from rest_framework import viewsets, generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
