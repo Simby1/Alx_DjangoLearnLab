f741560f0e56ffb0e628be3b2a78a11298ff3c4d
Simby1 <kimsimby@gmail.com>
2026-02-01 23:59:55 +0100

    feat: set up router - task 2

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import viewsets, generics # keep generics if you want both
 from .models import Book
 from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
