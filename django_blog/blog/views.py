75a66fa42afadf1bf8a5e33ba32241b2f4cd3fe5
Simby1 <kimsimby@gmail.com>
2026-02-23 21:24:00 +0100

    feat: complete views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -8,7 +8,43 @@ from .models import Post
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'  
+    context_object_name = 'posts'
     ordering = ['-published_date']
+
+# Show a single post
+class PostDetailView(DetailView):
+    model = Post
+
+# Create a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Update an existing post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Delete a post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 def register(request):
     if request.method == 'POST':
