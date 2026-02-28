dced540bcabafcac6615db49da0416447872f4d1
Simby1 <kimsimby@gmail.com>
2026-02-28 23:12:13 +0100

    feat: Add Comment Views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,9 @@ from .forms import CustomUserCreationForm
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
+from .models import Comment
+from .forms import CommentForm
+from django.urls import reverse_lazy
 
 class PostListView(ListView):
     model = Post
@@ -68,3 +71,39 @@ def profile(request):
         form = UserChangeForm(instance=request.user) 
     
     return render(request, 'blog/profile.html', {'form': form})
+
+# Comment Creation View
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        form.instance.post = Post.objects.get(pk=self.kwargs['pk'])
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['pk']})
+
+# Comment Update View
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.get_object().post.id})
+
+# Comment Delete View
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.get_object().post.id})
